9e39079508eb4efe6a7e0c5ae31e63b3c62b5c69
pat1005331 <pat1005331@users.noreply.github.com>
2018-08-29 18:56:07 +1000

    monitoring class

--- slot-in-smart-meter/slot-in-smart-meter/monitoring.h
+++ slot-in-smart-meter/slot-in-smart-meter/monitoring.h
@@ -3,11 +3,12 @@
 
 /*
 The CT transformer used it the Talema AS-103 it has a coil ratio of 300 : 1 and is rated to 15A. 
-Use a 51ohm resistor to convert the transformer current to a ref voltage to read with the arduino.
+Use a 40ohm resistor to convert the transformer current to a ref voltage to read with the arduino.
 */
 
 #include "config.h"
 #include "Arduino.h"
+#include <EmonLib.h>
 
 class monitoring
 {
@@ -16,12 +17,12 @@ public:
 	
 
 private:
+	
 	int CTPin;
-	float readCT();
-	float calculatePower(float);
+	int scaleFactor;
+	float calculatePower();
 
 	int voltage = 240;
-	int currentFactor;
 };
 
 
